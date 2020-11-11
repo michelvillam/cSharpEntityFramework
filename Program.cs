93beb940c2bea88f5323d9b75825186149e77901
miguelangel.navarro <miguelangel.navarro@bi-geek.com>
2020-11-11 13:25:32 +0100

    saving, updating and deleting examples

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using ReadData.Data;
+using System.Linq;
 
 namespace ReadData
 {
@@ -11,6 +12,36 @@ namespace ReadData
             Console.WriteLine("Starting the application. ");
 
             var db = new AppVentaCursoContext();
+
+            Instructor instructor = new Instructor
+            {
+                Nombre = "Lorenzo",
+                Apellidos = "Lopez",
+                Grado = "Master en computacion"
+            };
+
+            //***** SAVING INSTRUCTOR ********
+            //se añade el nuevo instructor al contexto
+            //db.Add(instructor);
+            //se inserta en BD. Devuelve el numero de transactiones que se han ejecutado en DB.
+            //int estadoTransaction = db.SaveChanges();
+
+            var instructorDB = db.Instructor.Single(p => p.Nombre == "Lorenzo");
+
+            //***** UPDATING INSTRUCTOR ********
+            /* if (instructorDB != null)
+            {
+                instructorDB.Grado = "Nuevo titulo";
+                db.SaveChanges();
+                Console.WriteLine("Update correctly");
+            } */
+
+            //***** DELETING INSTRUCTOR ********
+            if (instructorDB != null)
+            {
+                db.Remove(instructorDB);
+                db.SaveChanges();
+            }
 
         }
     }
