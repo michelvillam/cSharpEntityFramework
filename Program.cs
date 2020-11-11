9bbab4b87e9ec0fd966607e01c5425b35eccaa77
miguelangel.navarro <miguelangel.navarro@bi-geek.com>
2020-11-11 12:22:29 +0100

    clean program class

--- Program.cs
+++ Program.cs
@@ -12,28 +12,6 @@ namespace ReadData
 
             var db = new AppVentaCursoContext();
 
-            //Join tables with include. If there is N-M relation, you must use Include and ThenInclude
-            var cursos = db.Curso.Include(p => p.PrecioPromocion)
-                                 .Include(c => c.ComentarioLista)
-                                 .Include(i => i.CursoInstructors)
-                                    .ThenInclude(ci => ci.Instructor);// returns array IQuerable
-
-            foreach (var curso in cursos)
-            {
-                Console.WriteLine(curso.Titulo + " ---- " + curso.Description + " " + curso.PrecioPromocion.PrecioActual);
-
-                foreach (var comment in curso.ComentarioLista)
-                {
-                    Console.WriteLine(comment.Alumno + ": " + comment.ComentarioTexto);
-                }
-
-                foreach (var cursoInstructor in curso.CursoInstructors)
-                {
-                    Console.WriteLine("Instructor: " + cursoInstructor.Instructor.Nombre + " " + cursoInstructor.Instructor.Apellidos);
-                }
-
-            }
-
         }
     }
 }
